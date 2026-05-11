4175657108f0bb869e65b128fc8c8c53cf0ef594
Thanh Tâm <hoang.thanhtam.h3t@gmail.com>
2026-05-11 15:01:19 +0700

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,105 @@
+const ieltsData = {
+    part1: [
+        {
+            topic: "HOMES",
+            questions: [
+                "Do you live in a house or an apartment?",
+                "Which is your favourite room in your home? Why?",
+                "Would you change anything about your home? Why / why not?",
+                "What do you think makes a house feel like a home?",
+                "Would you like to move to a different home in the future?"
+            ]
+        },
+        {
+            topic: "SPORTS",
+            questions: [
+                "Do you like playing sports?",
+                "Do you think doing sports is a good way to relax?",
+                "What sports are popular in your country?",
+                "Do you think it is important to play a sport?"
+            ]
+        },
+        {
+            topic: "NEWS AND MEDIA",
+            questions: [
+                "Do you often read the news?",
+                "Where do you usually get the news from?",
+                "What kind of news do you usually follow?",
+                "In which kinds of circumstances do you usually listen to the radio?",
+                "Do you prefer reading online news or in newspapers?",
+                "What are the most credible media sources in your country?"
+            ]
+        },
+        {
+            topic: "TRAVELS AND TRANSPORTATION",
+            questions: [
+                "How do you go to work/school?",
+                "What's the most popular means of transportation in your hometown?",
+                "Do you think people will drive more in the future?",
+                "How often do you take buses?",
+                "Can you compare the advantages of planes and trains?",
+                "What form of transport do you prefer to use? Why?",
+                "What type of public transport can be found in your city?",
+                "Would you ride bikes to work in the future?",
+                "Where do you like to go on your vacation/holiday?"
+            ]
+        }
+    ],
+    part2: [
+        {
+            topic: "HOMES",
+            content: "Describe a house or apartment you would like to live in in the future.\n\nYou should say:\n- Where it is\n- What kind of accommodation it is\n- What it is like\n- And how you feel about this dream house/apartment."
+        },
+        {
+            topic: "SPORTS",
+            content: "Describe your favourite sport.\n\nYou should say:\n- What sport it is\n- How often you play it or watch it\n- How you play it\n- And explain why this is your favourite sport."
+        },
+        {
+            topic: "NEWS AND MEDIA",
+            content: "Describe a newspaper or magazine you enjoy reading.\n\n- what newspaper/ magazine it is\n- what the newspaper/ magazine is about\n- how often you read it\n- what kinds of people generally read this newspaper/ magazine\nand explain why you like to read it."
+        },
+        {
+            topic: "TRAVEL AND TRANSPORTATION",
+            content: "Describe a recent vacation that you went on.\n\n- Where and when did you go?\n- What did you do there?\n- How did you feel about it?"
+        }
+    ],
+    part3: [
+        {
+            topic: "HOMES",
+            questions: [
+                "Do you think homes will look different in the future?",
+                "How easy is it to find a place to live in your country?",
+                "Do you think it’s better to rent or to buy a place to live in?",
+                "Do you agree that there is a right age for young adults to stop living with their parents?",
+                "What factors will contribute to whether a place is good to live in or not?"
+            ]
+        },
+        {
+            topic: "SPORTS",
+            questions: [
+                "Do you think that competitive sports are damaging for children?",
+                "What characteristics do you think an athlete should have?",
+                "Why are there so few top athletes?",
+                "What’s the best way to become a top athlete?"
+            ]
+        },
+        {
+            topic: "NEWS AND MEDIA",
+            questions: [
+                "What kind of news do teenagers like to read? How about the old?",
+                "What's the difference between news on TV and news on the internet?",
+                "How has technology changed the way people get information?"
+            ]
+        },
+        {
+            topic: "TRAVEL AND TRANSPORTATION",
+            questions: [
+                "How do you think people will travel in the future?",
+                "How to encourage more people to take public transportation?",
+                "Which mode of transportation do you believe to be the safest? Why?",
+                "Should a nation’s government prioritize road or rail transportation more? Why?",
+                "What factors do people have to consider before a trip?"
+            ]
+        }
+    ]
+};
